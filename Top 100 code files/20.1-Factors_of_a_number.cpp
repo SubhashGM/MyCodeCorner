42e2264127770a55636fb18666303ad84cc2c415
SubhashGM <87770519+SubhashGM@users.noreply.github.com>
2023-10-13 09:21:38 +0530

    Create 20.1-Factors_of_a_number.cpp

---
+++ Top 100 code files/20.1-Factors_of_a_number.cpp
@@ -0,0 +1,14 @@
+//C++ program to find facotrs of a number using brute force
+#include<iostream>
+using namespace std;
+int main()
+{   int num;
+    cout<<"Enter any number: ";
+    cin>>num;
+    for(int i=1;i<=num;i++)
+    {
+        if(num%i==0)
+            cout<<i<<" ";
+    }
+    return 0;
+}
