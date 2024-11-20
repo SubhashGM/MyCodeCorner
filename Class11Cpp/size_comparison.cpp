7df500c75d158af7833401c04243a377dcd6f2af
SubhashGM <87770519+SubhashGM@users.noreply.github.com>
2024-11-20 06:02:56 +0530

    Add files via upload

---
+++ Class11Cpp/size_comparison.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Unsigned short: "<<sizeof(unsigned short)<<" byte"<<endl;
+    cout<<"Signed short: "<<sizeof(signed short)<<" byte"<<endl;
+    cout<<"Unsigned int: "<<sizeof(unsigned int)<<" byte"<<endl;
+    cout<<"Signed int: "<<sizeof(signed int)<<" byte"<<endl;
+    cout<<"Unsigned long: "<<sizeof(unsigned long int)<<" byte"<<endl;
+    cout<<"Signed long: "<<sizeof(long)<<" byte"<<endl;
+    cout<<"Char: "<<sizeof(char)<<" byte"<<endl;
+    cout<<"Float: "<<sizeof(float)<<" byte"<<endl;
+    cout<<"Double: "<<sizeof(double)<<" byte"<<endl;
+    cout<<"Double long: "<<sizeof(long double)<<" byte"<<endl;
+    return 0;
+}
