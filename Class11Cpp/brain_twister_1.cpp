2e1f52d5e1d346380dcc84f6c81df5d7f291052e
SubhashGM <87770519+SubhashGM@users.noreply.github.com>
2024-11-27 20:35:41 +0530

    Add files via upload

---
+++ Class11Cpp/brain_twister_1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n=4,k=2;
+    cout<<++n<<endl;
+    cout<<n<<endl;
+    cout<<n++<<endl;
+    cout<<n<<endl;
+    cout<<-n<<" "<<n<<endl;
+    cout<<--n<<endl;
+    cout<<n<<endl;
+    cout<<n--<<endl;
+    cout<<n+k<<endl;
+    cout<<n<<" "<<k<<endl;
+    return 0;
+}
---
+++ Class11Cpp/formatting_output.cpp
@@ -0,0 +1,10 @@
+#include<iomanip>
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<setw(8)<<22<<"\n";
+    cout<<setw(8)<<4444<<"\n";
+    cout<<setw(8)<<666666<<"\n";
+    return 0;
+}
